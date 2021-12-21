3d65f64556bb1fe583822f6c205bb75966c9fd0a
fahrizaall <fahrizalm84@gmail.com>
2021-12-21 11:05:07 +0700

    d

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,15 +8,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAxcrSeBpT0Q_VkEyob8_9uiPyRC3jHvFg",
-  authDomain: "promotbox-54e94.firebaseapp.com",
-  projectId: "promotbox-54e94",
-  storageBucket: "promotbox-54e94.appspot.com",
-  messagingSenderId: "855483451814",
-  appId: "1:855483451814:web:32f53ff2f63a3fdbd82767",
-  measurementId: "G-4914RDF8YF",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 // const analytics = getAnalytics(app);
